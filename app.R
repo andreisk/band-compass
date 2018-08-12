fcdffdc205f25dcc64aaadaf7d17575208af2241
Andrei <askopelevich@gmail.com>
2018-08-12 12:05:46 -0700

    Compass plot generator

--- app.R
+++ app.R
@@ -12,7 +12,7 @@ body = dashboardBody(
                   helpText("Right-click on the table to delete/insert rows/columns.", 
                            "Double-click on a cell to edit."),
                   br(),
-                  actionButton("save", "Save table")
+                  actionButton("save", "Save table/update plots")
                 ),
                 mainPanel(
                   rHandsontableOutput("hot")
@@ -20,10 +20,18 @@ body = dashboardBody(
               )
             )
             ),
-    tabItem(tabName = 'compass'#,
+    tabItem(tabName = 'compasses',
             #for every pair of vars, return a political compass
-            ),
-    tabItem(tabName = 'pca')
+            fluidPage(
+              uiOutput('comps')
+              )
+            )
+    ,
+    tabItem(tabName = 'pca',
+            fluidRow(
+              box(title = 'Biplot'),
+              box(title = 'Component Loadings')
+            ))
   )
 )
 
@@ -48,25 +56,60 @@ server = function(input, output){
     if (!is.null(input$hot)) {
       df = hot_to_r(input$hot)
     } else {
-       if (is.null(values[["df"]]))
+       if (is.null(values[['df']]))
          df <- df
        else
-        df <- values[["df"]]
+        df <- values[['df']]
     }
-    values[["df"]] <- df
+    values[['df']] <- df
   })
   
   output$hot <- renderRHandsontable({
-    df <- values[["df"]]
+    df <- values[['df']]
     if (!is.null(df))
       rhandsontable(df, useTypes = FALSE, stretchH = "all")
   })
   
-  ## Save 
+  # save/update/generate plots
   observeEvent(input$save, {
-    finaldf <- isolate(values[["df"]])
+    #save
+    finaldf <- isolate(values[['df']])
     saveRDS(finaldf, 'bands.RDS')
-  })
+    #compass plots
+    output$comps = renderUI({
+      lapply(2:dim(values[['df']])[2]-1,function(i){
+        output[[paste0('comp',i)]] = renderPlot({
+          ggplot(finaldf,
+                 aes(as.numeric(finaldf[,i]),
+                     as.numeric(finaldf[,i+1]),
+                     label=finaldf[,1])) + 
+            xlim(0,10) + 
+            ylim(0,10) + 
+            labs(x=colnames(finaldf)[i+1],
+                 y=colnames(finaldf)[i]) + 
+            annotate("rect", xmin = 5, xmax = 10, ymin = 5, ymax = 10, fill= "red")  + 
+            annotate("rect", xmin = 0, xmax = 5, ymin = 0, ymax = 5 , fill= "blue") + 
+            annotate("rect", xmin = 5, xmax = 10, ymin = 0, ymax = 5, fill= "yellow") + 
+            annotate("rect", xmin = 0, xmax = 5, ymin = 5, ymax = 10, fill= "green") +
+            geom_label() +
+            theme(axis.text.x=element_blank(),
+                  axis.text.y=element_blank(),
+                  axis.ticks.x=element_blank(),
+                  axis.ticks.y=element_blank(),
+                  panel.grid.major = element_blank(), 
+                  panel.grid.minor = element_blank()
+            ) +
+            scale_x_continuous(position ='top') +
+            scale_y_continuous(position = 'right')
+        })
+        plotOutput(paste0('comp',i))
+      })
+    })
+    #pca plots
+    #output$biplot
+    #output$loadings
+  }
+    )
 }
 
 shinyApp(ui, server)
